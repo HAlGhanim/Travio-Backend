abd9f3a1cea9e7162a187c46b2b3a5739ed5a1cd
nouftalajmi <alajminouf94@gmail.com>
2023-07-24 20:45:17 +0300

    done from the update and delete what's left is only testinfg the createdby after hmoud's work

--- api/Trips/trip.routes.js
+++ api/Trips/trip.routes.js
@@ -2,26 +2,27 @@ const express = require("express");
 const { getAllTrips, createTrip, upadateTrip, deleteTrip } = require("./trip.controllers");
 const Trip = require("../../models/Trip");
 const uploader = require("../../middlewares/uploader");
+const passport = require('passport')
 const router = express.Router();
 
-router.param("tripId", async (req, res, next, tripId) => {
-    try {
-        const trip = await Trip.findById(tripId);
-        if (!trip)
-            return res.status(404).json({
-                message: " no trip matches that ID!",
-            });
-        req.trip = trip;
-        next();
-    } catch (error) {
-        next(error);
-    }
-});
+// router.param("tripId", async (req, res, next, tripId) => {
+//     try {
+//         const trip = await Trip.findById(tripId);
+//         if (!trip)
+//             return res.status(404).json({
+//                 message: " no trip matches that ID!",
+//             });
+//         req.trip = trip;
+//         next();
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 router.get("/", getAllTrips);
 router.post("/", uploader.single("tripImage"), createTrip);
-router.put('/trips/:tripId', passport.authenticate('jwt', { session: false }), upadateTrip)
-router.delete('/trips/:tripId', passport.authenticate('jwt', { session: false }), deleteTrip)
+router.put('/:tripId', uploader.single("tripImage"), upadateTrip)
+router.delete('/:tripId', deleteTrip)
 
-
+// , passport.authenticate('jwt', { session: false })
 
 module.exports = router;
