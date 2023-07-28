adfd675364f85798d9a2e749b5c9b81bae8e8a29
MARIAM ALRASDAN <mariam.alrashdan97@gmail.com>
2023-07-28 23:17:57 +0300

    remove aithenticate from get trip by id

--- models/User.js
+++ models/User.js
@@ -1,21 +1,21 @@
 const { model, Schema } = require("mongoose");
 
 const userSchema = new Schema(
-    {
-        email: { type: String, unique: true, required: true },
-        username: { type: String, unique: true },
-        password: { type: String, required: true },
-        userImage: {
-            type: String,
-            //  default: "/",
-        },
-        bio: { type: String },
-
-        // relations
-        trips: [{ type: Schema.Types.ObjectId, ref: "Trip" }],
-        // countries: [{ type: Schema.Types.ObjectId, ref: "Country" }],
+  {
+    email: { type: String, unique: true, required: true },
+    username: { type: String, unique: true },
+    password: { type: String, required: true },
+    userImage: {
+      type: String,
+      default: "media/1690210343852cool-profile.jpeg",
     },
-    { timestamps: true }
+    bio: { type: String },
+
+    // relations
+    trips: [{ type: Schema.Types.ObjectId, ref: "Trip" }],
+    // countries: [{ type: Schema.Types.ObjectId, ref: "Country" }],
+  },
+  { timestamps: true }
 );
 
 module.exports = model("User", userSchema);
