6cb74961b1b0d020fe25ec61928080f924583aff
MARIAM ALRASDAN <mariam.alrashdan97@gmail.com>
2023-07-29 21:51:41 +0300

    location

--- models/Trip.js
+++ models/Trip.js
@@ -1,10 +1,14 @@
 const { model, Schema } = require("mongoose");
-// Everything with the word temp is a placeholder that you'll change in accordance with your project
 
 const TripSchema = new Schema({
   title: { type: String, unique: false, required: true },
   tripImage: { type: String, required: true },
   description: { type: String, unique: false, required: true },
+
+  // locationName: { type: String },
+  latitude: String,
+  longitude: String,
+
   // create relations in here and in the other model
   createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
